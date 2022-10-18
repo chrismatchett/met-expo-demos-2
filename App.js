b8524eaaee6caf659c2dfdb349d2187e0ff6dacf
Chris <chris@freeperiod.co.uk>
2022-10-18 18:57:21 +0100

    fixed expo docs url

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 function HomeScreen({navigation}) {
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
