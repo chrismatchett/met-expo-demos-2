2a8df1911246097639d6f5a0a3e9643345149061
Chris <chris@freeperiod.co.uk>
2022-12-06 12:13:38 +0000

    fetch api added to pizza app

--- app/pizza_welcome/index.js
+++ app/pizza_welcome/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, Text, Button, ActivityIndicator, FlatList } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
 function WelcomeScreen({ route, navigation }) {
 
   const { emailAddress} = route.params;
+
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+	   
+  const getPizzas = async () => {
+    try {
+        const response = await fetch('https://raw.githubusercontent.com/chrismatchett/met-expo-demos-2/main/assets/pizza.json');
+        const json = await response.json();
+        setData(json);
+        console.log(json);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+   }
+
+  const renderItem = ({ item }) => (
+    <Text>{item.name}, £{item.price}</Text>
+  );
+
+  useEffect(() => {
+    getPizzas();
+  }, []);
+
   
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text style={styles.heading1}>Welcome to Mario's</Text>
-      <Text style={styles.spacer} >We will send updates to {emailAddress}</Text>
-      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
-    </View>
+    <View style={styles.container}>
+      <View style={styles.section}>
+        <Text style={styles.heading1}>Welcome to Mario's</Text>
+        <Text style={styles.spacer} >We will send updates to {emailAddress}</Text>
+      </View>
+      <View style={styles.section}>
+        {isLoading ? <ActivityIndicator/> : (
+          <FlatList
+            data={data}
+	    keyExtractor={({ id }, index) => id}
+	    renderItem={renderItem}
+	  />
+	)} 
+      </View>
+   </View>
   );
 }
 
